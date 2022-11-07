91b1b0fd994743044c458185fd0587689897896a
akiid777 <akiid777@gmial.com>
2022-11-07 11:31:12 +0200

    Add Movie detail page and pass data

--- pages/superheros/[character]/[movie].tsx
+++ pages/superheros/[character]/[movie].tsx
@@ -1,6 +1,44 @@
+import { characters } from '../../../api/characters';
+import { useRouter } from 'next/router';
 
+type Parameters = {
+  movie: string;
+  character: string;
+};
+
+export const getStaticPaths = async () => {
+  const router = useRouter();
+  const query = router;
+  const paths: object = characters.map((character) => ({
+    params: {
+      character: character.route.toString(),
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({
+  params,
+}: {
+  params: Parameters;
+}) => {
+  console.log(params);
+  // let res: Array<object> = [];
+  // await apiInstance
+  //   .get(`/SearchTitle/k_4rgq7u85/${selectedHero[0].route}`)
+  //   .then((response) => {
+  //     if (response.data) {
+  //       res = response.data.results;
+  //       return res;
+  //     }
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //   });
+  return {};
+};
 
 export default function movie() {
-    return <div>Hi from movie</div>;
-  }
-  
+  return <div>Hi from movie</div>;
+}
--- pages/superheros/[character]/index.tsx
+++ pages/superheros/[character]/index.tsx
@@ -21,20 +21,29 @@ import {
   VisuallyHidden,
   List,
   ListItem,
+  Button,
 } from '@chakra-ui/react';
 import HerosCard from '../../../components/herosCard';
 import Link from 'next/link';
 import SimilerMoviesCard from '../../../components/similerMoviesCard';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 type Movie = {
   title: string;
   description: string;
   id: number;
   image: string;
+  resultType: string;
 };
 
 type Parameters = {
   character: string;
+};
+
+type Hero = {
+  route: string;
+  title: string;
+  image: string;
 };
 
 export const generateRandomNumber = (
@@ -61,17 +70,17 @@ export const getStaticProps = async ({
     (p) => p.route === params.character
   );
   let res: Array<object> = [];
-  // await apiInstance
-  //   .get(`/SearchTitle/k_4rgq7u85/${selectedHero[0].route}`)
-  //   .then((response) => {
-  //     if (response.data) {
-  //       res = response.data.results;
-  //       return res;
-  //     }
-  //   })
-  //   .catch((e) => {
-  //     console.log(e);
-  //   });
+  await apiInstance
+    .get(`/SearchTitle/k_4rgq7u85/${selectedHero[0].route}`)
+    .then((response) => {
+      if (response.data) {
+        res = response.data.results;
+        return res;
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
   return {
     props: {
       result: res,
@@ -80,118 +89,23 @@ export const getStaticProps = async ({
   };
 };
 
-export default function superHero(props: {
+export default function superHero({
+  result,
+  selectedHero,
+}: {
   result: Array<Movie>;
-  selectedHero: Array<object>;
+  selectedHero: Array<Hero>;
 }) {
-  // const [selectedMovie, setSelectedMovie] = useState<Movie>({
-  //   title: '',
-  //   description: '',
-  //   id: 0,
-  //   image: '',
-  // });
-  const result = [
-    {
-      id: 'tt11897478',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BYzhmNWMyYjQtNTVlMC00MGUwLWFmYjEtM2NkNWY0ODQ2YmFiXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_Ratio0.7273_AL_.jpg',
-      title: 'The Stranger',
-      description: '(II) (2022)',
-    },
-    {
-      id: 'tt4574334',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMDZkYmVhNjMtNWU4MC00MDQxLWE3MjYtZGMzZWI1ZjhlOWJmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7727_AL_.jpg',
-      title: 'Stranger Things',
-      description: '(2016) (TV Series)',
-    },
-    {
-      id: 'tt0077469',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMGYyMTAyMWItYzRhMC00NDQ5LWIzZjItNzM2ZmZlYmU0N2E1XkEyXkFqcGdeQXVyOTA0NzE2MzA@._V1_Ratio0.7273_AL_.jpg',
-      title: 'Dr. Strange',
-      description: '(1978) (TV Movie)',
-    },
-    {
-      id: 'tt10298840',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BZTk1NzQwMWQtNzVmNS00MDc4LWE1MTktMmQyYzExYWMwZDQ4XkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_Ratio0.7273_AL_.jpg',
-      title: 'Strange World',
-      description: '(2022)',
-    },
-    {
-      id: 'tt9698480',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BNWNlYzA2OWUtMWE1Mi00ZjMyLWExY2ItZDVlZmQ0YTMyZmEyXkEyXkFqcGdeQXVyMjYwNDA2MDE@._V1_Ratio0.7273_AL_.jpg',
-      title: 'The Stranger',
-      description: '(I) (2020) (TV Mini Series)',
-    },
-    {
-      id: 'tt1211837',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_Ratio0.7273_AL_.jpg',
-      title: 'Doctor Strange',
-      description: '(2016)',
-    },
-    {
-      id: 'tt0482606',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMTkxODAyODMwNV5BMl5BanBnXkFtZTcwNzk5Nzk2MQ@@._V1_Ratio0.7273_AL_.jpg',
-      title: 'The Strangers',
-      description: '(2008)',
-    },
-    {
-      id: 'tt8760932',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BYjBkYjgxNTktZjQ0Zi00OGU5LThjOTEtOGUzMDVhNjc2MjU4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg',
-      title: 'Nine Perfect Strangers',
-      description: '(2021) (TV Mini Series)',
-    },
-    {
-      id: 'tt9419884',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BNWM0ZGJlMzMtZmYwMi00NzI3LTgzMzMtNjMzNjliNDRmZmFlXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_Ratio0.7273_AL_.jpg',
-      title: 'Doctor Strange in the Multiverse of Madness',
-      description: '(2022)',
-    },
-    {
-      id: 'tt6461346',
-      resultType: 'Title',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BZWQ3MmFiNTctMzZkMS00NGY1LWI3ZTEtZjM4ZjJlY2VmMjY3XkEyXkFqcGdeQXVyNDU4MDQ0MjM@._V1_Ratio0.7273_AL_.jpg',
-      title: 'Stranger',
-      description: '(2017) (TV Series)',
-    },
-  ];
+  const [selectedMovie, setSelectedMovie] = useState<Movie>({});
+  useEffect(() => {
+    handleSelectedMovie();
+  }, []);
 
-  const selectedMovie = {
-    id: 'tt6461346',
-    resultType: 'Title',
-    image:
-      'https://m.media-amazon.com/images/M/MV5BZWQ3MmFiNTctMzZkMS00NGY1LWI3ZTEtZjM4ZjJlY2VmMjY3XkEyXkFqcGdeQXVyNDU4MDQ0MjM@._V1_Ratio0.7273_AL_.jpg',
-    title: 'Stranger',
-    description: '(2017) (TV Series)',
-  };
+  const handleSelectedMovie = () =>
+    setSelectedMovie(
+      result[generateRandomNumber(0, result.length - 1)]
+    );
 
-  // useEffect(() => {
-  //   setSelectedMovie(
-  //     props.result[generateRandomNumber(0, props.result.length - 1)]
-  //   );
-  // }, []);
-
-  // const handleSelectedMovie = () =>
-  //   setSelectedMovie(
-  //     props.result[generateRandomNumber(0, props.result.length - 1)]
-  //   );
   return (
     <div>
       <Nav />
@@ -235,6 +149,32 @@ export default function superHero(props: {
                 </Center>
                 <Center>
                   <Text>{selectedMovie.description}</Text>
+                </Center>
+                <Center>
+                  <Link
+                    href={{
+                      pathname: `/superheros/${selectedHero[0].route}/${selectedMovie.id}`,
+                      query: JSON.stringify([2, 3]),
+                    }}
+                  >
+                    <Button
+                      rightIcon={<ArrowForwardIcon />}
+                      colorScheme="teal"
+                      variant="outline"
+                    >
+                      See Movie Details
+                    </Button>
+                  </Link>
+                </Center>
+                <Center>
+                  <Button
+                    rightIcon={<ArrowForwardIcon />}
+                    colorScheme="teal"
+                    variant="outline"
+                    onClick={handleSelectedMovie}
+                  >
+                    Get Anther Random movie for the same Champion
+                  </Button>
                 </Center>
               </Box>
             </Box>
